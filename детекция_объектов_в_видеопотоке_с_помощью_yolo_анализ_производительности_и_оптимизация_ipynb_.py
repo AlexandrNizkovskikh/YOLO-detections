5a5ee8a4ade3481400ddd92f484b650138c0ce38
Alexandr Nizkovskikh <79976593+AlexandrNizkovskikh@users.noreply.github.com>
2025-02-06 07:59:43 +0300

    Update детекция_объектов_в_видеопотоке_с_помощью_yolo_анализ_производительности_и_оптимизация_ipynb_.py

--- детекция_объектов_в_видеопотоке_с_помощью_yolo_анализ_производительности_и_оптимизация_ipynb_.py
+++ детекция_объектов_в_видеопотоке_с_помощью_yolo_анализ_производительности_и_оптимизация_ipynb_.py
@@ -8,6 +8,34 @@ Original file is located at
 
 # Детекция объектов в видеопотоке с помощью YOLO: анализ производительности и оптимизация
 
+## 🚀 Технологический стек
+_____________
+
+### 🖥 Языки программирования  
+![Python](https://img.shields.io/badge/-Python-3776AB?logo=python&logoColor=white&style=for-the-badge)
+
+### 📦 Фреймворки и библиотеки  
+![Ultralytics YOLO](https://img.shields.io/badge/-YOLO-00FFFF?logo=yolo&logoColor=black&style=for-the-badge)  
+![FiftyOne](https://img.shields.io/badge/-FiftyOne-FF4081?logo=python&logoColor=white&style=for-the-badge)  
+![OpenCV](https://img.shields.io/badge/-OpenCV-5C3EE8?logo=opencv&logoColor=white&style=for-the-badge)  
+![NumPy](https://img.shields.io/badge/-NumPy-013243?logo=numpy&logoColor=white&style=for-the-badge)  
+![Pillow](https://img.shields.io/badge/-Pillow-3776AB?logo=python&logoColor=white&style=for-the-badge)  
+![TQDM](https://img.shields.io/badge/-TQDM-FFA500?logo=python&logoColor=white&style=for-the-badge)  
+
+### ⚡ Оптимизация и ускорение  
+![FP16](https://img.shields.io/badge/-FP16-6E4C13?logo=nvidia&logoColor=white&style=for-the-badge)  
+![Batch Processing](https://img.shields.io/badge/-Batch%20Processing-4CAF50?logo=python&logoColor=white&style=for-the-badge)  
+![FFmpeg](https://img.shields.io/badge/-FFmpeg-007808?logo=ffmpeg&logoColor=white&style=for-the-badge)  
+
+### 📂 Работа с данными  
+![VisDrone2019-VID](https://img.shields.io/badge/-VisDrone2019-VID-blue?logo=databricks&logoColor=white&style=for-the-badge)  
+![Tabulate](https://img.shields.io/badge/-Tabulate-FF6F00?logo=python&logoColor=white&style=for-the-badge)  
+
+### 🔧 Среда разработки и инструменты  
+![Google Colab](https://img.shields.io/badge/-Google%20Colab-F9AB00?logo=googlecolab&logoColor=white&style=for-the-badge)  
+![gdown](https://img.shields.io/badge/-gdown-3776AB?logo=google-drive&logoColor=white&style=for-the-badge)  
+![Subprocess](https://img.shields.io/badge/-Subprocess-6A5ACD?logo=python&logoColor=white&style=for-the-badge)  
+
 ###Краткая аннотация
 
 В данной работе исследуется использование моделей YOLO для детекции объектов в видеопотоке. Были проведены показательные эксперименты, включающие инференс базовых моделей (YOLOv3, YOLOv5, YOLOv8, YOLOv11) в интерфейсе **FiftyOne** с выводом метрик точности: **precision**, **recall**, **f1-score**, **support**, а также средние значения (**micro/macro/weighted avg**).
